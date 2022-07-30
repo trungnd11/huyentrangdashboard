5f78d6c13f4b01f122bcde954284a58ac9fd9e26
trungnd11 <trungnd1@devcamp.edu.vn>
2022-07-30 14:01:55 +0700

    Update bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import "bootstrap/dist/css/bootstrap.css";
+
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  return <div className="App">
+    
+  </div>;
 }
 
 export default App;
