0c81b696e52634d6821ac9dc664466e3564572f8
trungnd9 <96941235+trungnd9@users.noreply.github.com>
2022-09-18 11:04:55 +0700

    Fix url

--- src/api/baseUrl.ts
+++ src/api/baseUrl.ts
@@ -1,5 +1,5 @@
 
-// const url = "https://huyentrangbeautycenter.herokuapp.com";
-const url = "http://localhost:5500";
+const url = "https://huyentrangbeautycenter.herokuapp.com";
+// const url = "http://localhost:5500";
 
 export default url;
